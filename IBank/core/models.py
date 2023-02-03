07a392338e1100d67ddb3614d35e88b73560a7c7
Marcelo-L-Valerio <mar.valerio@hotmail.com.br>
2023-02-03 16:32:11 -0300

    Core app/ User model

---
+++ IBank/core/models.py
@@ -0,0 +1,88 @@
+
+import os
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ValidationError
+
+
+class UserManager(BaseUserManager):
+    '''Manager for users'''
+
+    def create(self, email, password=None, **extra_fields):
+        '''Create, save and return a new user'''
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        '''Create and return a new superuser'''
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''User registration of basic and personal informations'''
+
+    NATURAL_PERSON = 'PF'
+    LEGAL_ENTITY = 'PJ'
+    USER_OPTS = [
+        (NATURAL_PERSON, 'Fisica'),
+        (LEGAL_ENTITY, 'Juridica'),
+    ]
+
+    owner_type = models.CharField(max_length=2, choices=USER_OPTS, default='PF')
+    cpf = models.CharField(max_length=11, unique=True, null=True)
+    cnpj = models.CharField(max_length=14, unique=True, null=True)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'name'
+
+    def clean(self):
+        '''Validation: an user can't be registered without at least one address'''
+        if len(self.address) == 0:
+            raise ValidationError('At least one address is required')
+        super(User, self).clean()
+
+
+class Account(models.Model):
+    '''Account model, related to an user, witch cotains all
+    bancary informations from the user'''
+
+    class Meta:
+        unique_together = (('account', 'agency'), )
+
+    CHECKING_ACCOUNT = 'CC'
+    SAVINGS_ACCOUNT = 'CP'
+    ACCOUNT_OPTS = [
+        (CHECKING_ACCOUNT, 'Corrente'),
+        (SAVINGS_ACCOUNT, 'Poupanca'),
+    ]
+
+    user = models.OneToOneField('user', on_delete=models.CASCADE)
+    agency = models.CharField(max_length=5)
+    account = models.CharField(max_length=7)
+    account_type = models.CharField(max_length=2, choices=ACCOUNT_OPTS, default='CC')
+    is_active = models.BooleanField(default=True)
+    balance = models.IntegerField(default=0)
+    register_date = models.DateField(auto_now_add=True, blank=True)
+
+
+class Address(models.Model):
+    '''Address registration on database'''
+
+    user = models.ForeignKey('user', on_delete=models.CASCADE)
+    state = models.CharField(max_length=2)
+    city = models.CharField(max_length=45)
+    district = models.CharField(max_length=45)
+    zip_code = models.CharField(max_length=8)
+    street = models.CharField(max_length=255)
---
+++ IBank/core/tests/test_model.py
@@ -0,0 +1,95 @@
+
+from django.test import TestCase
+from core.models import User, Account, Address
+
+
+def create_user(cpf='12345678910', email='test@example.com',
+                name='TestUser', password='Testpass123'):
+    '''Assist function for agile user creation'''
+    return User.objects.create(cpf=cpf, email=email, name=name, password=password)
+
+
+class UserAppTests(TestCase):
+
+    def test_create_user(self):
+
+        payload = {
+            'cpf': '12345678910',
+            'email': 'test@example.com',
+            'name': 'TestUser',
+        }
+        password = 'Testpass123'
+        user = User.objects.create(**payload, password=password)
+
+        for param, value in payload.items():
+            self.assertEqual(getattr(user, param), value)
+        self.assertTrue(user.check_password(password))
+
+    def test_cnpj_user(self):
+
+        payload = {
+            'owner_type': 'PJ',
+            'cnpj': '12345678910123',
+            'email': 'test@example.com',
+            'name': 'TestUser',
+            'password': 'Testpass123'
+        }
+        user = User.objects.create(**payload)
+
+        self.assertEqual(user.cnpj, '12345678910123')
+        self.assertIsNone(user.cpf)
+
+    def test_create_account(self):
+
+        user = create_user()
+
+        payload = {
+            'user': user,
+            'agency': '12345',
+            'account': '1234567',
+        }
+
+        account = Account.objects.create(**payload)
+
+        for param, value in payload.items():
+            self.assertEqual(getattr(account, param), value)
+
+    def test_create_account_same_number_defferent_agency(self):
+
+        user1 = create_user()
+        user2 = create_user(
+            name='Test2', email='Test2@example.com',cpf='12345678911'
+        )
+        payload1 = {
+            'user': user1,
+            'agency': '12345',
+            'account': '1234567',
+        }
+        payload2 = {
+            'user': user2,
+            'agency': '12346',
+            'account': '1234567',
+        }
+
+        account1 = Account.objects.create(**payload1)
+        account2 = Account.objects.create(**payload2)
+
+        self.assertNotEqual(account1.agency, account2.agency)
+
+    def test_address_creation(self):
+
+        user = create_user()
+
+        payload = {
+            'user': user,
+            'state': 'SP',
+            'city': 'São Paulo',
+            'district': 'Zona Leste',
+            'zip_code': '11111000',
+            'street': 'Flower St, 7329',
+        }
+
+        address = Address.objects.create(**payload)
+
+        for param, value in payload.items():
+            self.assertEqual(getattr(address, param), value)
